e53d9e6c472433be73ec3c98deeccd242d1e6320
Sejun9909 <choisejun99@gmail.com>
2023-03-29 19:09:04 +0900

    보드 2개 -> 1개로 수정

--- app.py
+++ app.py
@@ -154,9 +154,5 @@ def myinfo():
 def mypage():
     return render_template('mypage.html')
 
-@app.route('/borad')
-def borde():
-    return render_template('board.html')
-
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
